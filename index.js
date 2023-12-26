f90981df5966db4b5301f40453651e5142d7f107
Belay Birhanu <104232857+adgehTech@users.noreply.github.com>
2023-12-26 21:21:48 +0300

    Update index.js
    one time commit

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  // Access uploaded file information
+  const file = req.upfile;
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded." });
+  }
 
+  // Process file information
+  const fileInfo = {
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  };
 
+  // Respond with JSON containing file information
+  res.json(fileInfo);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
